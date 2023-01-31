7d14ad1f749f2d81186db5d72752f23736a6b59b
Alfonso <algagu01@floridauniversitaria.es>
2023-01-31 16:28:42 +0100

    correcion menu

--- menu.sh
+++ menu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "BIENVENIDO/A AL JUEGO DE PIEDRA PAPEL TIJERA"
-echo "=============================================="
+echo "BIENVENIDO/A AL JUEGO DE PIEDRA PAPEL TIJERA LAGARTO SPOCK"
+echo "=========================================================="
 echo "1.- INICIAR JUEGO"
 echo "2.- VER REGLAS"
 echo "3.- SALIR DEL JUEGO"
