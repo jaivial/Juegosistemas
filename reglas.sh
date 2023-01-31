508f6fbcb6ca93fc0e80962380a2ed9841378b91
Jacobo-curras14 <jacusa@floridauniversitaria.es>
2023-01-31 16:44:25 +0100

    reglas del juego

---
+++ reglas.sh
@@ -0,0 +1,13 @@
+#!/bin/bashecho 
+echo "REGLAS DEL JUEGO"
+echo "================"
+echo "Tijera corta a papel"
+echo "Tijera decapita a lagarto"
+echo "Papel tapa a piedra"
+echo "Papel desautoriza a Spock"
+echo "Piedra aplasta a lagarto"
+echo "Piedra aplasta a tijera"
+echo "Lagarto envenena a Spock"
+echo "Lagarto devora a papel"
+echo "Spock rompe a tijera"
+echo "Spock vaporiza a piedra"
