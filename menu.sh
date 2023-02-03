1ae94d3799de7478faad545cabce97e7b6b720a9
Jacobo-curras14 <jacusa@floridauniversitaria.es>
2023-02-03 15:48:37 +0100

    menu finalizado

--- menu.sh
+++ menu.sh
@@ -5,3 +5,23 @@ echo "=========================================================="
 echo "1.- INICIAR JUEGO"
 echo "2.- VER REGLAS"
 echo "3.- SALIR DEL JUEGO"
+read -p "Selecciona una opción: " eleccion
+
+
+##Switch case que da un valor a la variable elegida por el usuario.
+case "$eleccion" in 
+"1")
+    echo "Iniciando Partida"
+    source ./juego.sh
+    ;;
+"2")
+    echo "Abriendo Reglas"
+    source ./reglas.sh
+    ;;
+"3")
+    echo "Saliendo del Juego."
+    ;;
+*)
+    echo "La opción introducida no es correcta, teclea un número del 1 al 3."
+    ;;
+esac
