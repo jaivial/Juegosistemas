826e81b988edbbe63ed1f15dd2ea9cafb127cb20
Jaime Villanueva <javial@floridauniversitaria.es>
2023-02-03 16:52:41 +0100

    forma correcta de abrir otro archivo.sh + clear + sleep

--- menu.sh
+++ menu.sh
@@ -12,14 +12,20 @@ read -p "Selecciona una opción: " eleccion
 case "$eleccion" in 
 "1")
     echo "Iniciando Partida"
-    source ./juego.sh
+    sleep 1
+    clear
+    bash juegopruebas.sh
     ;;
 "2")
     echo "Abriendo Reglas"
-    source ./reglas.sh
+    sleep 1
+    clear
+    bash reglaspruebas.sh
     ;;
 "3")
     echo "Saliendo del Juego."
+    sleep 1
+    clear
     ;;
 *)
     echo "La opción introducida no es correcta, teclea un número del 1 al 3."
